95659fcb77630279870d9c60705a53e3c6789d1a
Jyriu <sami.yezza@gmail.com>
2024-11-12 18:56:04 +0100

    Add Like model for posts and replies; update model imports and migration

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -3,6 +3,7 @@ from extensions import db
 from .user import User
 from .post import Post
 from .reply import Reply
+from .like import Like
 
 
-__all__ = ['User', 'Post', 'Reply']
+__all__ = ['User', 'Post', 'Reply', 'Like']
